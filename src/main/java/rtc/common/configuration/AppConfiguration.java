4e148cc3056799d8dd6b8ac0c9c16a420b472c1c
Sandeep18 <bhaskarla.sandeep@gmail.com>
2024-12-16 17:27:40 +0530

    Configured Swagger. But not added Security.

--- src/main/java/rtc/common/configuration/AppConfiguration.java
+++ src/main/java/rtc/common/configuration/AppConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+
 @Configuration
 public class AppConfiguration {
 
@@ -27,4 +28,12 @@ public class AppConfiguration {
     public WebClient.Builder webClient(){
         return WebClient.builder();
     }
+
+    // @Bean
+    // public GroupedOpenApi publicApi() {
+    //     return GroupedOpenApi.builder()
+    //             .group("springboot-public-apis")
+    //             .pathsToMatch("/api/**")
+    //             .build();
+    // }
 }
